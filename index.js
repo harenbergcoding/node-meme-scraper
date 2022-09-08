eed33ac82d364aa9b55ba89bb7f57d68b4bf1a16
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-09-08 16:57:31 +0200

    Added step 3 + 4

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
-const response = await fetch('https://memegen-link-examples-upleveled.netlify.app/');
-const body = await response.text(); // convert to string
-const imgSrc = body.split( 'src' );
-console.log(imgSrc);
+const response = await fetch('https://memegen-link-examples-upleveled.netlify.app');
+const body = await response.text(); // convert url to string
+console.log(body);
 
+const $ = cheerio.load(body); // load string into cherio
 
+const imageUrl = []; // outside of the funtion otherwise you get each array for every url
+
+$('img', body).each(function () { // copy img src into one array
+  const image = $(this).attr('src');
+  imageUrl.push(image);
+});
+
+// console.log(imageUrl); => output one array
+
+const imageUrlFirst10 = [];
+
+for (let i = 0; i < 10; i++) {
+  imageUrlFirst10.push(imageUrl[i]);
+}
+
+// console.log(imageUrlFirstTen); output one array with first ten img src
