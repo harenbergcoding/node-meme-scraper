48575d8346eab5dbc8f37e3e97f1159fb26971b2
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-09-09 16:02:09 +0200

    preflight check

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ for (let i = 0; i < 10; i++) {
 
 // console.log(imageUrlFirstTen); output one array with first ten img src
 
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/01.jpg', imageUrlFirst10[0]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/02.jpg', imageUrlFirst10[1]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/03.jpg', imageUrlFirst10[2]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/04.jpg', imageUrlFirst10[3]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/05.jpg', imageUrlFirst10[4]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/06.jpg', imageUrlFirst10[5]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/07.jpg', imageUrlFirst10[6]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/08.jpg', imageUrlFirst10[7]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/09.jpg', imageUrlFirst10[8]);
-fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper-/memes/10.jpg', imageUrlFirst10[9]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/01.jpg', imageUrlFirst10[0]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/02.jpg', imageUrlFirst10[1]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/03.jpg', imageUrlFirst10[2]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/04.jpg', imageUrlFirst10[3]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/05.jpg', imageUrlFirst10[4]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/06.jpg', imageUrlFirst10[5]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/07.jpg', imageUrlFirst10[6]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/08.jpg', imageUrlFirst10[7]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/09.jpg', imageUrlFirst10[8]);
+fs.writeFileSync('c:/Users/haren/projects/node-meme-scraper/memes/10.jpg', imageUrlFirst10[9]);
 
 
 
