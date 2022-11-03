894b65db7d237c3062279d88e013ba0ba45ee675
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-11-03 20:02:25 +0100

    Add defined path end, create path and memes folder

--- index.js
+++ index.js
@@ -1,70 +1,88 @@
 import * as cheerio from 'cheerio';
 import * as fs from 'fs';
+import { mkdir } from 'fs';
 import fetch from 'node-fetch';
+
+const imageUrls = [];
+const imageUrlFirst10 = [];
 
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app',
 );
-const body = await response.text(); // convert url to string
-console.log(body);
+// converts html body into string
+const body = await response.text();
 
-const $ = cheerio.load(body); // load string into cherio
+// loads string into cherio
+const $ = cheerio.load(body);
 
-const imageUrl = []; // outside of the funtion otherwise you get each array for every url
-
+// fetches image urls and saves them into the array
 $('img', body).each(function () {
-  // copy img src into one array
-  const image = $(this).attr('src');
-  imageUrl.push(image);
+  const images = $(this).attr('src');
+
+  imageUrls.push(images);
 });
 
-// console.log(imageUrl); => output one array
-
-const imageUrlFirst10 = [];
-
+// gets first ten image urls
 for (let i = 0; i < 10; i++) {
-  imageUrlFirst10.push(imageUrl[i]);
+  imageUrlFirst10.push(imageUrls[i]);
 }
 
-// console.log(imageUrlFirstTen); output one array with first ten img src
+// Defined path end
+const pathEnd = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
 
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/01.jpg',
-  imageUrlFirst10[0],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/02.jpg',
-  imageUrlFirst10[1],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/03.jpg',
-  imageUrlFirst10[2],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/04.jpg',
-  imageUrlFirst10[3],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/05.jpg',
-  imageUrlFirst10[4],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/06.jpg',
-  imageUrlFirst10[5],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/07.jpg',
-  imageUrlFirst10[6],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/08.jpg',
-  imageUrlFirst10[7],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/09.jpg',
-  imageUrlFirst10[8],
-);
-fs.writeFileSync(
-  'c:/Users/haren/projects/node-meme-scraper/memes/10.jpg',
-  imageUrlFirst10[9],
-);
+// create non existing path
+const path = `./memes/${pathEnd}.jpg`;
+
+// Creates /memes folder
+fs.mkdir('./memes', { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+// Download url to folder
+
+// fs.createWriteStream(imageUrlFirst10[0]);
+
+// fs.writeFile(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/01.jpg',
+//   imageUrlFirst10[0],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/02.jpg',
+//   imageUrlFirst10[1],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/03.jpg',
+//   imageUrlFirst10[2],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/04.jpg',
+//   imageUrlFirst10[3],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/05.jpg',
+//   imageUrlFirst10[4],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/06.jpg',
+//   imageUrlFirst10[5],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/07.jpg',
+//   imageUrlFirst10[6],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/08.jpg',
+//   imageUrlFirst10[7],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/09.jpg',
+//   imageUrlFirst10[8],
+// );
+// fs.writeFileSync(
+//   'c:/Users/haren/projects/node-meme-scraper/memes/10.jpg',
+//   imageUrlFirst10[9],
+// );
+
+// const imgfetch = await fetch(
+//   'https://api.memegen.link/images/xy/all_the_things!!!.jpg?width=300',
+// );
